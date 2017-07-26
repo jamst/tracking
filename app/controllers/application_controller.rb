a26867640a0d3250666031d4aaa89ccbff8a37a9
jamst <jamstjamst@163.com>
2017-07-26 19:54:20 +0800

    add opxpid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,43 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_employee!
-  before_action :set_cookie
+  before_action :update_or_create_opxpid
 
-  def set_cookie
-  	unless cookies[:uuid]
-      cookies[:uuid] = "11111111"
+
+  def update_or_create_opxpid
+  	begin
+      @cookie_domain = request.env["HTTP_X_DOMAIN_FOR"].to_s.strip unless request.env["HTTP_X_DOMAIN_FOR"].to_s.strip == ""
+    rescue
+    ensure
+      @cookie_domain = ".whmall.test" unless @cookie_domain
     end
+
+    if cookies[:opxPID].to_i > 2000
+      opxpid = cookies[:opxPID].to_i.to_s
+      cookies[:opxPID] = {
+        :value => "",
+        :expires => Time.now
+      }
+      cookies[:opxPID] = {
+        :value => opxpid,
+        :expires => 2.years.from_now,
+        :domain => @cookie_domain
+      }
+    else
+      host_ip = request.env['SERVER_NAME'].to_s.gsub(/(\.|:)/, '')
+
+      t_format = Time.now.strftime('%Y%m%d%H%M%S%6N')
+      randid = "%08d" % (rand*100000000).to_i
+
+      opxpid = "#{t_format}#{host_ip}#{randid}"
+
+      cookies[:opxPID] = {
+        :value => opxpid,
+        :expires => 2.years.from_now,
+        :domain => @cookie_domain
+      }
+    end
+    return opxpid
   end
 
 end
--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -2,14 +2,24 @@ class TrackingsController < ActionController::Base
   # js请求	
   def index
   	#p CGI.unescape(params[:opxcookie]) 
+    begin
+      @cookie_domain = request.env["HTTP_X_DOMAIN_FOR"].to_s.strip unless request.env["HTTP_X_DOMAIN_FOR"].to_s.strip == ""
+    rescue
+    ensure
+      @cookie_domain = ".whmall.test" unless @cookie_domain
+    end
 
   	tracking_hash = {}
-  	tracking_hash[:rnum] = params[:rnum]
+    # 本机作为第三方的cookie
+    tracking_hash[:opxPID] = cookies[:opxPID]
+    # 宿主网站cookie
     tracking_hash[:time_now] = Time.now
-    tracking_hash[:domain] = params[:domain] 
+    tracking_hash[:domain] = @cookie_domain 
     tracking_hash[:opxtitle] = params[:opxtitle] 
     tracking_hash[:opxreferrer] = params[:opxreferrer] 
     tracking_hash[:opxurl] = params[:opxurl] 
+    tracking_hash[:opxid] = params[:opxid] 
+    tracking_hash[:ip] = request.env['HTTP_X_FORWARDED_FOR'] || request.env['HTTP_X_REAL_IP'] || request.remote_ip
     tracking_hash[:opxuserAgent] = CGI.unescape(params[:opxuserAgent]) 
 
     opxcookie = CGI.unescape(params[:opxcookie])
@@ -17,25 +27,51 @@ class TrackingsController < ActionController::Base
     opxcookie_hash = {}
     opxcookie.each do |c|
       cc = c.strip	
-	  if cc.include? "="
-	  	ccc = cc.split("=")
-	    opxcookie_hash[ccc[0]] = ccc[1]
-	  end  
+  	  if cc.include? "="
+  	  	ccc = cc.split("=")
+  	    opxcookie_hash[ccc[0]] = ccc[1]
+  	  end  
     end
 
     tracking_hash[:opxcookie] = opxcookie_hash
+    # 日志处理
+
+    # 定义受访页面代号设置
+
+    # mysql只保留当天（开启里实时追踪的用户）实时数据，历史数据在日志里每天清洗入库。
+
+    # 存储到数据库中，id关联
+
+    # 根据url来触发响应
+    if params[:opxurl].include?("temporary_reports")
+      p 111
+      # 产品热点记录叠加统计（产品分类，与到访客户）
+      # 某个页面的受访热点（页面热点分布图）
+    else
+      p 222
+    end
+
+    # 当天上线通知
+    if @book_uuids.include? cookies[:opxPID]
+      # opxPID当天页面list(a,b,c,d)
+    else
+      # 当天在线人数＋1（高频人数，低频人数）（买家人数，卖家人数）
+    end
+
+    # 是否首次记录
+    if @databases_uuids.include? cookies[:opxPID]
+      unless params[:opxid].to_i >= 31415926 * 201314
+         # 绑定 opxid & opxPID & domain
+      end
+    else
+      # 
+    end
+
+
 
 
     Tracking << tracking_hash
-
-    # 实时处理
-      
-      # 平台粒度
-      # 客户粒度
-      # 员工粒度  
-
-    # 日志处理
-    render :json => "32132"
-
+    
+    return render :js => ""
   end
 end
