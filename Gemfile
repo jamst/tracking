a8d29907e7330b08835c0f425eb7672b04f644a4
jamst <jamstjamst@163.com>
2017-07-28 00:08:30 +0800

    添加desboard

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ end
 # source 'https://gems.ruby-china.org'
 
 gem 'rails', '>= 5.0.0.rc2', '< 5.1'
-gem 'puma', '3.6.2'
+gem 'puma'
 gem 'jbuilder', '~> 2.5'
 gem 'rake'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :trackings
+  resources :desboard
 
   resources :temporary_reports do
     collection do 
@@ -26,7 +27,7 @@ Rails.application.routes.draw do
     end
   end
 
-  root to: 'temporary_reports#index'
+  root to: 'desboard#index'
   devise_for :employees, path: "admin", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', sign_up: 'cmon_let_me_in' }, controllers: { sessions: "admin/sessions", passwords: "admin/passwords"}
   match '/admin/ajax_bar' => 'website/ajax_bar/ajax_bar#ajax', :as => :ajax_bar, :via => [:post, :get]
 
