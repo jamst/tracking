fad054729458bd4f0782b999ada96d301bede570
jamst <jamstjamst@163.com>
2017-07-29 22:11:10 +0800

    控制面板设置

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :trackings
-  resources :desboard
+  resources :desboard do
+    collection do
+      get :analysis
+    end
+  end
 
   resources :temporary_reports do
     collection do 
