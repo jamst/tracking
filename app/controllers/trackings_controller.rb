829f9597a5398b7744a26643b39aed5029f089e3
jamst <jamstjamst@163.com>
2017-07-31 18:02:24 +0800

    添加log页面处理

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -34,49 +34,35 @@ class TrackingsController < ActionController::Base
     end
 
     tracking_hash[:opxcookie] = opxcookie_hash
-    # 日志处理
-
-    # 定义受访页面代号设置
-
-    # mysql只保留当天（开启里实时追踪的用户）实时数据，历史数据在日志里每天清洗入库。
-
-    # 存储到数据库中，id关联
     
 
     # 是否首次记录
     if cache_read("uuids_list").include? cookies[:opxPID]
       # 统计游客url浏览记录
-      cache_write("#{cookies[:opxPID]}_url_list",params[:opxurl])
+      cache_array("#{cookies[:opxPID]}_url_list",params[:opxurl])
+
       if params[:opxid].to_i >= 31415926 * 201314
-        
-      else  
-        # 绑定 opxid & opxPID & domain
+        # 当前用户已登录状态
+        cache_value(cookies[:opxPID],params[:opxid].to_i) 
       end
     else
       # uuid添加进数组
       cache_array("uuids_list",cookies[:opxPID])
       # uuids数统计加一
-      cache_write("uuids")
+      cache_sum("uuids")
     end
 
 
-    # 当天上线通知
-    if @book_uuids.include? cookies[:opxPID]
-      # opxPID当天页面list(a,b,c,d)
-    else
-      # 当天在线人数＋1（高频人数，低频人数）（买家人数，卖家人数）
-    end
 
     # 根据url来触发响应
     if params[:opxurl].include?("temporary_reports")
-      p 111
       # 产品热点记录叠加统计（产品分类，与到访客户）
       # 某个页面的受访热点（页面热点分布图）
-    else
+    elsif params[:opxurl].include?("temporary_reports")
       p 222
     end
 
-
+    # 日志处理
     Tracking << tracking_hash
     
     return render :js => ""
