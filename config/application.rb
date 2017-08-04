d9bc54d0e1e06d26a36c73c0a10e3e1b482f33de
jamst <jamstjamst@163.com>
2017-08-04 11:56:30 +0800

    添加缓存方法

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,40 @@ module PreReport
     config.autoload_paths += Dir["#{config.root}/lib"]
   end
 end
+
+
+# 缓存汇总
+  def cache_sum(tag)
+    if Rails.cache.read(tag).blank? 
+        Rails.cache.write(tag, 1)
+    else
+      tags = Rails.cache.read(tag)
+      Rails.cache.write(tag, tags + 1)
+    end
+  end
+
+  # 缓存储值对
+  def cache_value(tag,value)
+    Rails.cache.write(tag, value)
+  end
+
+  # 缓存数组
+  def cache_array(tag,arr,uniq=nil)
+    if Rails.cache.read(tag).blank? 
+      Rails.cache.write(tag, [arr])
+    else
+      tags = Rails.cache.read(tag)
+      unless uniq && (tags.include? arr)
+        tags << arr
+        Rails.cache.write(tag, tags) 
+      end
+    end
+  end
+
+  # 读缓存
+  def cache_read(tag)
+    Rails.cache.fetch(tag)||[]
+  end
+
+
+  TRACKING_TAG = {"dashboard":"首页","crm/companies":"公司管理","inquiry/inquiries":"询盘","inquiry/inquiries/":"报价","order/details":"订单","finance/reconciliations":"财务"}
