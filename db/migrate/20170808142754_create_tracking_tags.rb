c40b8114933c6fe7ced2f99ae59c53c3808d47fc
李江锋 <lijiangfeng@lijiangfengdeMacBook-Pro.local>
2017-08-11 07:50:33 +0800

    添加追踪初始化设置&实时追踪标记

--- db/migrate/20170808142754_create_tracking_tags.rb
+++ db/migrate/20170808142754_create_tracking_tags.rb
@@ -1,10 +1,10 @@
 class CreateTrackingTags < ActiveRecord::Migration[5.0]
   def change
     create_table :tracking_tags do |t|
-       t.string  "dimain"
-       t.string  "link_name"
-       t.string  "tag_name"	
-       t.string  "contect"
+       t.string  "dimain", comment: "对应网站"
+       t.string  "link_name", comment: "对应连接"
+       t.string  "tag_name"	, comment: "对应热点名称"
+       t.string  "contect"	, comment: "简述"
        t.datetime "created_at", null: false, comment: "创建日期"
        t.datetime "updated_at", null: false, comment: "修改日期"
     end
