44b48d5904cfe1575817ac03aa482a7a11597a10
jamst <jamstjamst@163.com>
2017-08-14 22:52:46 +0800

    添加明细记录&缓存数据持久化到mysql

--- app/models/tracking_record.rb
+++ app/models/tracking_record.rb
@@ -1,3 +1,22 @@
 class TrackingRecord < ApplicationRecord
 
+ def self.load_tracking
+   # 缓存导入数据库	
+   TrackingRecord.transaction do
+       cache_read("uuids_list").each do |_| 
+         message = cache_read("#{_}_message")	
+         TrackingRecord.create(opxpid:_,opxid:message[:opxid],time_now:message[:time_now],domain:message[:domain],
+         	                   opxtitle:message[:opxtitle],opxreferrer:message[:opxreferrer],opxurl:message[:opxurl],ip:message[:ip],
+         	                   opxuserAgent:message[:opxuserAgent])
+       end	
+	   cache_read("uuids_list").each do |_|
+	      cache_read("#{_}_url_list").each do |url|
+	        TrackingDetail.create(opxpid:_,opxurl:url)
+	      end
+	   end
+   end
+   # 清除缓存
+   delete_cache
+ end
+
 end
