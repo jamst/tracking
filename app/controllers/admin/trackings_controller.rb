b5e5f2c286a49a65c8ced7ff598227ee04d22590
jamst <jamstjamst@163.com>
2017-08-18 13:27:57 +0800

    用户信息为空兼容

--- app/controllers/admin/trackings_controller.rb
+++ app/controllers/admin/trackings_controller.rb
@@ -37,8 +37,8 @@ class Admin::TrackingsController < ApplicationController
   def load_detail
     @employee = Employee.find(params[:em_id].to_i) if params[:em_id].to_i >0
     @opxpid = params[:opxpid]
-    @message = cache_read("#{@opxpid}_message")
-    @urls = cache_read("#{@opxpid}_url_list").reverse
+    @message = cache_read("#{@opxpid}_message")||{}
+    @urls = cache_read("#{@opxpid}_url_list").reverse||[]
     valid_keys = cache_read("#{@opxpid}_tags")
     @tags = TRACKING_TAG.slice(*valid_keys).values.to_s
   end
