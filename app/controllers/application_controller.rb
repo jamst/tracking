b735963227f00a553f606772666fcd9bd7998423
jamst <lijiangfeng@lijiangfengdeMacBook-Pro.local>
2017-10-22 09:23:33 +0800

    add employee

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_employee!
-  before_action :update_or_create_opxpid
+  layout "application" 
 
 
   def update_or_create_opxpid
